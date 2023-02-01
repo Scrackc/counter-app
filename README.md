2e655afc86e9e267e14d7bf536ec43099536186e
Eduardo Perez <prz.rmz.eduardo@gmail.com>
2023-02-01 13:41:02 -0600

    app de contador con StatefulWidget

---
+++ README.md
@@ -0,0 +1,10 @@
+# counter_app
+
+Aplicación de contador.
+
+### Acciones en la app
+1. Incrementar en 1
+2. Reiniciar el contador en 0
+3. Decrementar en 1
+
+
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    const fontSize30 = TextStyle(fontSize: 30);
+    int counter = 0;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomeScreen"),
+        elevation: 1,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Numero de Clicks", style: fontSize30),
+            Text('$counter', style: fontSize30 ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => {
+          counter++,
+          print("hello $counter"),
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
+    );
+  }
+}
